5f5d438c427e647081abda70d8befa58034d46e4
Anmol Desai <anmoldesai4@gmail.com>
2019-08-13 23:13:55 +1000

    added and removed note functiona added with chalk

--- notes.js
+++ notes.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const addNote = (title, body) => {
-    const notes = loadNotes();
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(note => note.title === title);
+
+  if (duplicateNotes.length === 0) {
     notes.push({
-        title,
-        body
+      title,
+      body
     });
     saveNotes(notes);
+  } else {
+    console.log("cannot add a duplicate note");
+  }
 };
 
 const getNote = title => {
@@ -17,21 +24,29 @@ const getAll = () => {
   console.log('get all notes');
 };
 
-const removeNote = title => {
-  console.log('removing note');
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(note => {
+    return note.title !== title;
+  });
+
+  if(notes.length > notesToKeep.length){
+    console.log(chalk.green.inverse("Note removed"));
+    saveNotes(notesToKeep);
+  }else{
+    console.log(chalk.red.inverse("No note found"));
+  }
 };
 
 const saveNotes = (notes) => {
-    console.log(notes);
-    fs.writeFileSync('notes.json', JSON.stringify(notes));
+  fs.writeFileSync('notes.json', JSON.stringify(notes));
 }
 
 const loadNotes = () => {
   try {
-    const dataJson = JSON.parse(fs.readFileSync('notes.json'));
-    return dataJson;
+    return JSON.parse(fs.readFileSync('notes.json'));
   } catch (e) {
-      return [];
+    return [];
   }
 };
 
